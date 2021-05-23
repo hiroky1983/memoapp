3fb42093bb1396e8c04a12a8ec0cc681325aed6e
yamadahiroki <hirockysan1983@gmail.com>
2021-05-24 01:04:08 +0900

    firebaseの設定とデータをfirestoreに渡す処理まで実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
 import Head from "next/head";
 import { useCallback, useState } from "react";
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
-
 import "tailwindcss/tailwind.css";
+
 import Header from "../components/Header";
 import InputForms from "../components/InputForm";
 import MemosTheme from "../components/MemosTheme";
-import { Firestore } from "@google-cloud/firestore";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/functions";
+import { firebaseConfig } from "../firebase/Config";
+
+firebaseConfig;
 
 export default function Home() {
   const [inputText, setInputText] = useState("");
   const [themes, setThemes] = useState([]);
 
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+  const functions = firebase.functions();
+  const FirebaseTimestamp = firebase.firestore.Timestamp;
+
   const onChangeInputText = (e) => setInputText(e.target.value);
 
-  const onClickAdd = () => {
+  const onClickAdd = async () => {
     if (inputText === "") return;
     const newThemes = [...themes, inputText];
+    
     if (themes.some((item) => item === inputText)) {
       alert("同じ題名があります");
       return inputText;
@@ -29,11 +39,14 @@ export default function Home() {
       alert("題名を入力して下さい");
       return inputText;
     }
+    await db.collection("memo").add({
+      theme: newThemes,
+    });
     setThemes(newThemes);
     setInputText("");
   };
 
-  const keyDown = (e) => {
+  const keyDown = async (e) => {
     if (e.keyCode === 13) {
       const newThemes = [...themes, inputText];
       if (themes.some((item) => item === inputText)) {
@@ -44,6 +57,9 @@ export default function Home() {
         alert("題名を入力して下さい");
         return inputText;
       }
+      await db.collection("memo").add({
+        theme: newThemes,
+      });
       setThemes(newThemes);
       setInputText("");
     }
