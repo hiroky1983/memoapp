75ff1229b56fd837e52d1d8754c392a928f156d2
yamadahiroki <hirockysan1983@gmail.com>
2021-06-06 01:26:56 +0900

    firebaseのバージョンアップ

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,28 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
-// import "firebase/functions";
+import firebase, { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-export const firebaseConfig = {
+const firebaseapp = initializeApp({
   apiKey: "AIzaSyCgFBr_0kCsozpzQtlsFkmV94AJxIQ3W2o",
   authDomain: "memoapp-6586b.firebaseapp.com",
   databaseURL:
     "https://memoapp-6586b-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "memoapp-6586b",
-  storageBucket: "memoapp-6586b.appspot.com",
   messagingSenderId: "145419180898",
   appId: "1:145419180898:web:6081dcf29307fe4b80aa3b",
   measurementId: "G-FYLHZ19JPH",
-};
+});
 
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const storage = firebase.storage();
-// export const functions = firebase.functions();
-export const FirebaseTimestamp = firebase.firestore.Timestamp;
-export default firebase;
+export const db = getFirestore();
