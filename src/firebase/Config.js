382c9208819adec7fc16fddbbf83b9c7a54155e1
yamadahiroki <hirockysan1983@gmail.com>
2021-06-10 23:26:14 +0900

    .env導入と環境変数設定

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -2,15 +2,20 @@ import firebase, { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCgFBr_0kCsozpzQtlsFkmV94AJxIQ3W2o",
-  authDomain: "memoapp-6586b.firebaseapp.com",
-  databaseURL: "https://memoapp-6586b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "memoapp-6586b",
-  storageBucket: "memoapp-6586b.appspot.com",
-  messagingSenderId: "145419180898",
-  appId: "1:145419180898:web:6081dcf29307fe4b80aa3b",
-  measurementId: "G-FYLHZ19JPH"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: "G-FYLHZ19JPH",
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 export const db = firebase.firestore();
