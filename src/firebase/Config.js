dd030fbbb531312c5a3e5656574c474b869c25e4
yamadahiroki <hirockysan1983@gmail.com>
2021-06-10 22:25:27 +0900

    firebaseのバージョン変更に伴う記述変更

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,7 +1,7 @@
 import firebase, { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-export const firebaseapp = initializeApp({
+export const firebaseConfig = {
   apiKey: "AIzaSyCgFBr_0kCsozpzQtlsFkmV94AJxIQ3W2o",
   authDomain: "memoapp-6586b.firebaseapp.com",
   databaseURL: "https://memoapp-6586b-default-rtdb.europe-west1.firebasedatabase.app",
@@ -10,6 +10,7 @@ export const firebaseapp = initializeApp({
   messagingSenderId: "145419180898",
   appId: "1:145419180898:web:6081dcf29307fe4b80aa3b",
   measurementId: "G-FYLHZ19JPH"
-});
+};
 
-export const db = getFirestore();
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
